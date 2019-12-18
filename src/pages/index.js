5e72ccacf3c49652e56e34ab2025f955b543946f
Alexander Fountain <alexander@digett.com>
2019-12-18 17:04:27 -0600

    sidebar styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,45 +3,64 @@ import { Link, graphql } from "gatsby";
 import Masonry from "react-masonry-component";
 import Img from "gatsby-image";
 import Layout from "../components/layout";
-import { Fade } from 'react-slideshow-image';
+import { Fade } from "react-slideshow-image";
+import BackgroundImage from "gatsby-background-image";
 
- 
 const fadeProperties = {
   duration: 5000,
   transitionDuration: 500,
   infinite: false,
-  indicators: true,
-}
+  indicators: false,
+  arrows: false
+};
 
 const IndexPage = ({ data }) => (
-  <Layout>
-  <Fade {...fadeProperties}>
-  {data.home.nodes[0].homeHeroSlideshow.map((slide, index) => (
-    <div>
-    <img src={slide.url} key={index} />
-    </div>
-  ))}
-  </Fade>
-    <Masonry className="showcase">
-      {data.venue.edges.map(({ node: venue }) => (
-        <div key={venue.id} className="showcase__item">
-          <figure className="card">
-            <Link to={`/venue/${venue.slug}`} className="card__image">
-              <Img fluid={venue.coverImage.fluid} />
-            </Link>
-            <figcaption className="card__caption">
-              <h6 className="card__title">
-                <Link to={`/venue/${venue.slug}`}>{venue.title}</Link>
-              </h6>
-              <div className="card__description">
-                <p>{venue.excerpt}</p>
-              </div>
-            </figcaption>
-          </figure>
+  <div>
+    <div className="home-slide">
+      <Fade {...fadeProperties}>
+        {data.home.nodes[0].homeHeroSlideshow.map((slide, index) => (
+          <div className="each-slide">
+            <BackgroundImage
+              className="slide"
+              fluid={slide.fluid}
+            ></BackgroundImage>
+          </div>
+        ))}
+      </Fade>
+      <div className="overlay-container">
+        <div className="overlay-top">Your next adventure awaits!</div>
+        <div className="overlay">
+          <p>
+            Elephantnose fish bluegill longfin rice eel, skipping goby king of
+            herring lefteye flounder. Glassfish, searobin ricefish deep sea
+            anglerfish Arctic char North American darter.
+          </p>
         </div>
-      ))}
-    </Masonry>
-  </Layout>
+      </div>
+    </div>
+    <Layout>
+      <h2 className="venues-title">Our Venues</h2>
+      <Masonry className="showcase">
+        {data.venue.edges.map(({ node: venue }) => (
+          <div key={venue.id} className="showcase__item">
+            <figure className="card">
+              <Link to={`/venue/${venue.slug}`} className="card__image">
+                <Img fluid={venue.coverImage.fluid} />
+              </Link>
+              <figcaption className="card__caption">
+                <h6 className="card__title">
+                  <Link to={`/venue/${venue.slug}`}>{venue.title}</Link>
+                </h6>
+                <div className="card__description">
+                  <p>{venue.excerpt}</p>
+                </div>
+              </figcaption>
+            </figure>
+          </div>
+        ))}
+      </Masonry>
+    </Layout>
+  </div>
 );
 
 export default IndexPage;
@@ -64,11 +83,21 @@ export const query = graphql`
       }
     }
     home: allDatoCmsHome {
-    nodes {
-      homeHeroSlideshow {
-        url
+      nodes {
+        homeHeroSlideshow {
+          url
+          fluid(maxWidth: 1920) {
+            base64
+            tracedSVG
+            aspectRatio
+            width
+            height
+            src
+            srcSet
+            sizes
+          }
+        }
       }
     }
-  }
   }
 `;
